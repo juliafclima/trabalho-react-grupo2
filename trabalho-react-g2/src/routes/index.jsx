99f65f120e2ba4fe98ab9f9fbe5318d8a72462e0
juliafclima <juliafclima@hotmail.com>
2023-11-02 11:37:45 -0300

    feat: estruturas de pastas

---
+++ trabalho-react-g2/src/routes/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+export default function AppRouter() {
+  return (
+    <div>
+      <Routes>
+         <Route></Route>
+      </Routes>
+    </div>
+  )
+}
