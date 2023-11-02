22303744242aba6b32f2f8ad98634acd9b1ee83f
Hevelise - Liz <liz.souza.dias@gmail.com>
2023-11-02 15:22:11 -0300

    Criação de pastas e arquivos

--- trabalho-react-g2/src/App.jsx
+++ trabalho-react-g2/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routes";
 
 export default function App() {
-  return (
+  return    (
     <>
     {/* tag 1.0.0 */}
       <BrowserRouter>
