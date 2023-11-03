0695cb8b9fb4069a7a3ba067e0977ca5f51719e4
juliafclima <juliafclima@hotmail.com>
2023-11-03 10:31:33 -0300

    build: subindo versao

--- trabalho-react-g2/src/App.jsx
+++ trabalho-react-g2/src/App.jsx
@@ -4,7 +4,7 @@ import AppRouter from "./routes";
 export default function App() {
   return    (
     <>
-    {/* tag 2.0.0 */}
+    {/* tag 2.1.0 */}
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
