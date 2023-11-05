beaaa719e9dec52ac5a52cf2b110e9ac1e902b61
juliafclima <juliafclima@hotmail.com>
2023-11-05 16:51:21 -0300

    feat: depoimento dinamico mas sem foto e nome

---
+++ trabalho-react-g2/src/pages/Posts/index.jsx
@@ -0,0 +1,80 @@
+import { useForm } from "react-hook-form";
+
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import "./style.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Header from "../../components/Header";
+
+
+const validationPost = yup.object().shape({
+  titulo: yup.string().required("Preencha o título").max(40, "Até 40 caract."),
+  conteudo: yup
+    .string()
+    .required("Preencha o conteúdo")
+    .max(300, "Até 300 caract."),
+});
+
+export default function Posts() {
+  let navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors } } = useForm({ resolver: yupResolver(validationPost) });
+
+  const addPost = (data) => {
+    axios
+      .post("https://6542dfe001b5e279de1fabce.mockapi.io/posts", data)
+      .then(() => {
+        console.log('Deu certo!');
+        navigate('/depoimentos');
+      })
+      .catch(() => {
+        alert('Deu errado!');
+      });
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <div className="card-post">
+          <h1>Criar depoimento</h1>
+          <hr />
+          <div className="card-body-post">
+            <form action="#" onSubmit={handleSubmit(addPost)}>
+              <div className="fields">
+                <label htmlFor="titulo">Titulo</label>
+                <input
+                  type="text"
+                  id="titulo"
+                  name="titulo"
+                  {...register("titulo")}
+                />
+                <p className="error-message">{errors.titulo?.message}</p>
+              </div>
+
+              <div className="fields">
+                <label htmlFor="conteudo">Conteúdo</label>
+                <textarea
+                  cols="30"
+                  rows="10"
+                  type="text"
+                  id="conteudo"
+                  {...register("conteudo")}
+                ></textarea>
+                <p className="error-message">{errors.conteudo?.message}</p>
+              </div>
+
+              <div className="btn-post">
+                <button>Enviar</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
--- trabalho-react-g2/src/routes/index.jsx
+++ trabalho-react-g2/src/routes/index.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Depoimentos from '../pages/Depoimentos';
 import Comunidade from '../pages/Comunidade';
 import Album from '../pages/Album';
 import Erro from '../pages/Erro';
 import Login from '../pages/Login';
 import Cadastro from '../pages/Cadastro';
 import Perfil from '../pages/Perfil';
-import DepoimentosPage from '../pages/Depoimentos/DepoimentosPage';
+import Posts from './../pages/Posts/index';
+import Update from '../pages/Update';
 
 export default function AppRouter() {
   return (
     <div>
       <Routes>
         < Route path="/" element={< Perfil />} />
-        <Route path="/depoimentos" component={DepoimentosPage} />
+        < Route path="/posts" element={< Posts />} />
+        < Route path="/depoimentos" element={< Depoimentos />} />
         < Route path="/comunidade" element={< Comunidade />} />
         < Route path="/album" element={< Album />} />
         < Route path="*" element={< Erro />} />
         < Route path="/login" element={< Login />} />
         < Route path="/cadastro" element={< Cadastro />} />
+        <Route path="/update/:id" element={<Update />} />
       </Routes>
     </div>
   );
