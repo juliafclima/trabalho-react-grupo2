e2b690852bcecbe73797983cd4e6f7daf218522f
Hevelise - Liz <liz.souza.dias@gmail.com>
2023-11-05 11:30:21 -0300

    Feat: Atualização de comunidade.

--- trabalho-react-g2/src/pages/Comunidade/index.jsx
+++ trabalho-react-g2/src/pages/Comunidade/index.jsx
@@ -1,47 +1,298 @@
 import React from "react";
 import "./style.css";
-import Capa from "../../assets/img/comunidade/carlucho.png";
-import Lupa from "../../assets/img/comunidade/lupa.jpg";
-import violao from "../../assets/img/comunidade/violao.jpg";
+import borracha from "../../assets/img/comunidade/comun1.png";
+import garfield from "../../assets/img/comunidade/comun2.png";
+import thony from "../../assets/img/comunidade/comun3.png";
+import sorvete from "../../assets/img/comunidade/comun4.png";
+import xororo from "../../assets/img/comunidade/comun5.png";
+import shampoo from "../../assets/img/comunidade/comun6.png";
+import comun7 from "../../assets/img/comunidade/comun7.png";
+import comun8 from "../../assets/img/comunidade/comun8.png";
+
+
+import Header from '../../components/Header';
 
 export default function Comunidade() {
   return (
     <>
       <Header />
       <div className="comunidade">
-        <div className="card">
-          <div className="cardimg">
-            <img className="morangola" src={Capa} alt="" />
-          </div>
-          <div className="texto">
-            <h1>Eu amo morango! </h1>
-            <p>
-              Esta comunidade é pra quem ama morangos, máscaras de morangos, doces de morangos, cabelos cor de morango e cheirinho de morango.
-            </p>
-          </div>
-        </div>
-        <div className="card">
-          <div className="cardimg">
-            <img src={violao} alt="" />
-          </div>
-          <div className="texto">
-            <h1>Eu toco mesmo, e daí!? </h1>
-            <p>
-              Esta comunidade é pra quem tem violões, camisas de violões, bolsinha de violões, chapéu de violões, chinelo de violões e tatuagem de violões.
-            </p>
-          </div>
-        </div>
-        <div className="card">
-          <div className="cardimg">
-            <img src={Lupa} alt="" />
-          </div>
-          <div className="texto">
-            <h1>Professor, aumenta a letra!? </h1>
-            <p>
-              Esta comunidade é pra quem tem 20 anos mas enxerga menos que um senhor de 324 anos.
-            </p>
-          </div>
-        </div>
+
+
+
+
+
+      <div className="card">
+  <div className="cardimg">
+    
+  </div>
+  <div className="texto">
+  </div>
+
+
+  {/* Card dentro do card */}
+  <div className="subCard">
+
+  <img src={borracha} alt="" />
+
+
+    <h2></h2>
+
+    <p>Eu nunca terminei uma borracha.</p>
+    
+  </div>
+
+<div>
+
+  <h1>Eu colho flores e TONY RAMOS.</h1>
+    <p>
+      Esta comunidade é pra quem tem violões, camisas de violões, bolsinha de violões, chapéu de violões, chinelo de violões e tatuagem de violões.
+    </p>
+    </div>
+</div>
+        
+
+
+
+
+
+
+<div className="card">
+  <div className="cardimg">
+    
+  </div>
+  <div className="texto">
+  </div>
+
+
+  {/* Card dentro do card */}
+  <div className="subCard">
+
+  <img src={garfield} alt="" />
+
+
+    <h2></h2>
+
+    <p>Eu colho flores e THONY RAMOS.</p>
+    
+  </div>
+
+<div>
+
+  <h1>Eu colho flores e TONY RAMOS.</h1>
+    <p>
+      Esta comunidade é pra quem tem violões, camisas de violões, bolsinha de violões, chapéu de violões, chinelo de violões e tatuagem de violões.
+    </p>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+<div className="card">
+  <div className="cardimg">
+    
+  </div>
+  <div className="texto">
+  </div>
+
+
+  {/* Card dentro do card */}
+  <div className="subCard">
+
+  <img src={thony} alt="" />
+
+
+    <h2></h2>
+
+    <p>Eu colho flores e THONY RAMOS.</p>
+    
+  </div>
+
+<div>
+
+  <h1>Eu colho flores e TONY RAMOS.</h1>
+    <p>
+      Esta comunidade é pra quem tem violões, camisas de violões, bolsinha de violões, chapéu de violões, chinelo de violões e tatuagem de violões.
+    </p>
+    </div>
+
+
+</div>
+
+
+
+
+<div className="card">
+  <div className="cardimg">
+    
+  </div>
+  <div className="texto">
+  </div>
+
+
+  {/* Card dentro do card */}
+  <div className="subCard">
+
+  <img src={sorvete} alt="" />
+
+
+    <h2></h2>
+
+    <p>Eu colho flores e THONY RAMOS.</p>
+    
+  </div>
+
+<div>
+
+  <h1>Eu colho flores e TONY RAMOS.</h1>
+    <p>
+      Esta comunidade é pra quem tem violões, camisas de violões, bolsinha de violões, chapéu de violões, chinelo de violões e tatuagem de violões.
+    </p>
+    </div>
+</div>
+
+
+
+
+<div className="card">
+  <div className="cardimg">
+    
+  </div>
+  <div className="texto">
+  </div>
+
+
+  {/* Card dentro do card */}
+  <div className="subCard">
+
+  <img src={xororo} alt="" />
+
+
+    <h2></h2>
+
+    <p>Eu colho flores e THONY RAMOS.</p>
+    
+  </div>
+
+<div>
+
+  <h1>Eu colho flores e TONY RAMOS.</h1>
+    <p>
+      Esta comunidade é pra quem tem violões, camisas de violões, bolsinha de violões, chapéu de violões, chinelo de violões e tatuagem de violões.
+    </p>
+    </div>
+</div>
+
+
+
+
+
+
+<div className="card">
+  <div className="cardimg">
+    
+  </div>
+  <div className="texto">
+  </div>
+
+
+  {/* Card dentro do card */}
+  <div className="subCard">
+
+  <img src={shampoo} alt="" />
+
+
+    <h2></h2>
+
+    <p>Eu colho flores e THONY RAMOS.</p>
+    
+  </div>
+
+<div>
+
+  <h1>Eu colho flores e TONY RAMOS.</h1>
+    <p>
+      Esta comunidade é pra quem tem violões, camisas de violões, bolsinha de violões, chapéu de violões, chinelo de violões e tatuagem de violões.
+    </p>
+    </div>
+</div>
+
+
+
+
+
+<div className="card">
+  <div className="cardimg">
+    
+  </div>
+  <div className="texto">
+  </div>
+
+
+  {/* Card dentro do card */}
+  <div className="subCard">
+
+  <img src={comun7} alt="" />
+
+
+    <h2></h2>
+
+    <p>Eu colho flores e THONY RAMOS.</p>
+    
+  </div>
+
+<div>
+
+  <h1>Eu colho flores e TONY RAMOS.</h1>
+    <p>
+      Esta comunidade é pra quem tem violões, camisas de violões, bolsinha de violões, chapéu de violões, chinelo de violões e tatuagem de violões.
+    </p>
+    </div>
+</div>
+
+
+
+
+
+<div className="card">
+  <div className="cardimg">
+    
+  </div>
+  <div className="texto">
+  </div>
+
+
+  {/* Card dentro do card */}
+  <div className="subCard">
+
+  <img src={comun8} alt="" />
+
+
+    <h2></h2>
+
+    <p>Eu colho flores e THONY RAMOS.</p>
+    
+  </div>
+
+<div>
+
+  <h1>Eu colho flores e TONY RAMOS.</h1>
+    <p>
+      Esta comunidade é pra quem tem violões, camisas de violões, bolsinha de violões, chapéu de violões, chinelo de violões e tatuagem de violões.
+    </p>
+    </div>
+</div>
+
+
+
+
+
+
       </div >
     </>
   );
