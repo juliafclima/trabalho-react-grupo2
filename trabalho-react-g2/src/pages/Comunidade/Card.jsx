d89caa94b8d5db5f6bb9f7fae6c6432bf08388c1
Hevelise - Liz <liz.souza.dias@gmail.com>
2023-11-06 01:29:28 -0300

    Feat:atualização de css da comunidade (quase no jeito)

--- trabalho-react-g2/src/pages/Comunidade/Card.jsx
+++ trabalho-react-g2/src/pages/Comunidade/Card.jsx
@@ -1,48 +1,63 @@
 import React from "react";
 import "./style.css";
 
-const Card = ({ imgSrc, titulo, descricao, idioma, categoria, tipo, membro, dono, data, }) => {
-   return (
-      <div className="cardComunidade">
-         <div className="subCardComunidade">
-                  <img src={imgSrc} alt="" />
-                  <p> <strong>{titulo}</strong> </p>
-                  <p>({membro} membros)</p>
-                  <hr />
-            <p className="cardLinkComunidade">✔️ participar</p>
-            <p className="cardLinkComunidade">👥 convidar amigos</p>
-            <p className="cardLinkComunidade">🚷 denunciar abuso</p>
-               </div>
+const Card = ({
+  imgSrc,
+  titulo,
+  descricao,
+  idioma,
+  categoria,
+  tipo,
+  membro,
+  dono,
+  data,
+  piquininim,
+}) => {
+  return (
+    <div className="cardComunidade">
+      <div className="subCardComunidade">
+        <img src={imgSrc} alt="" />
+        <p>
+          {" "}
+          <strong>{titulo}</strong>{" "}
+        </p>
+        <p>({membro} membros)</p>
+        <hr />
+        <p className="cardLinkComunidade">✔️ participar</p>
+        <p className="cardLinkComunidade">👥 convidar amigos</p>
+        <p className="cardLinkComunidade">🚷 denunciar abuso</p>
+      </div>
 
-               <div className="textoComunidade">
-                  <h1>{titulo}</h1>
-                  <p>
-                     <span>descrição: </span>
-                     {descricao}
-                  </p>
-                  <p>
-                     <span>idioma: </span>
-                     {idioma}
-                  </p>
-                  <p>
-                     <span>categoria: </span>
-                     {categoria}
-                  </p>
-                  <p>
-                     <span>tipo: </span>
-                     {tipo}
-                  </p>
-                  <p>
-                     <span>dono: </span>
-                     {dono}
-                  </p>
-                  <p>
-                     <span>data: </span>
-                     {data}
-                  </p>
-               </div>
-            </div>
-   );
+      <div className="textoComunidade">
+        <h1>{titulo}</h1>
+        <p className="piquininim">{piquininim}</p>
+        <p>
+          <span>descrição: </span>
+          {descricao}
+        </p>
+        <p>
+          <span>idioma: </span>
+          {idioma}
+        </p>
+        <p>
+          <span>categoria: </span>
+          {categoria}
+        </p>
+        <p>
+          <span>tipo: </span>
+          {tipo}
+        </p>
+        <p>
+          <span>dono: </span>
+          {dono}
+        </p>
+        <p>
+          <span>data: </span>
+          {data}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
